6ef9d9ee930eb160a62c895af162ef14312e273e
Alexander Barbara <aybarbara@gmail.com>
2017-12-13 09:42:55 -0500

    add Query#fetch

--- app/models/query.rb
+++ app/models/query.rb
@@ -3,6 +3,10 @@ class Query < ApplicationRecord
   validates :repo, presence: true
   validate :allowed_event_type
 
+  def fetch(page = 1)
+    Github::Processor.process(self, page)
+  end
+
   def allowed_event_type
     return true if event_type.blank?
     errors.add(:event_type, "not allowed") unless Github::Event.valid?(event_type)
