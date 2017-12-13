5205dfdf0c30d94cefdb098a7bda8776972273e5
Alexander Barbara <aybarbara@gmail.com>
2017-12-13 06:00:11 -0500

    add Query model validations and specs

--- app/models/query.rb
+++ app/models/query.rb
@@ -1,2 +1,11 @@
 class Query < ApplicationRecord
+  validates :owner, presence: true
+  validates :repo, presence: true
+  validate :allowed_event_type
+
+  def allowed_event_type
+    return true if event_type.blank?
+    errors.add(:event_type, "not allowed") unless Github::Event.valid?(event_type)
+  end
+
 end
--- spec/models/query_spec.rb
+++ spec/models/query_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Query, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :owner }
+  it { should validate_presence_of :repo }
+
+  describe "event type" do
+    before(:each) do
+      @query = Query.new(owner: "rails", repo: "rails")
+    end
+
+    it "should allow blank event type" do
+      expect(@query.event_type).to be_nil
+      expect(@query).to be_valid
+    end
+
+    it "should allow valid event such as ForkEvent" do
+      @query.event_type = "ForkEvent"
+      expect(@query).to be_valid
+    end
+
+    it "should not allow invalid events" do
+      @query.event_type = "xyz"
+      expect(@query).to be_invalid
+    end
+
+  end
 end
