f6555e9e336d975eb86402e1b912a1acc657ce84
Alexander Barbara <aybarbara@gmail.com>
2017-12-13 09:42:46 -0500

    add error summary helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def error_summary(obj)
+    out = []
+    obj.errors.messages.each do |field, errors|
+      errors.each do |error|
+        out.push("#{field.capitalize} #{error}")
+      end
+    end
+    out.join(", ")
+  end
+
 end
